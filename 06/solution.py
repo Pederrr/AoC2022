1864c2557911f0e34c50ae2aaab17d4a30f4a21c
Peter Romančík <romancikpeter@gmail.com>
2023-04-18 21:10:38 +0200

    feat(06): Add solution

---
+++ 06/solution.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+
+def find_marker(datastream: str, marker_size: int) -> int:
+    last_four = deque(datastream[0:marker_size])
+    for i in range(marker_size, len(datastream)):
+        last_four.append(datastream[i])
+        last_four.popleft()
+
+        if len(set(last_four)) == marker_size:
+            return i + 1
+    
+    return -1
+
+
+def solve(file: str) -> None:
+    datastream = ""
+    with open(file, "r") as input:
+        datastream = input.readline().strip()
+        
+    print("1: ", find_marker(datastream, 4))
+    print("2: ", find_marker(datastream, 14))
+
+        
+    pass
+
+
+if __name__ == "__main__":
+    solve("./input.txt")
+
